a969e976dd7ae351727244eca69b94911cb4d2fa
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2025-04-25 22:40:43 +0800

    refactor: foreign key for account

--- src/db/schema/scheduled_posts.ts
+++ src/db/schema/scheduled_posts.ts
@@ -2,13 +2,16 @@ import { index, integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { sql } from "drizzle-orm";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { z } from "zod";
+import { accounts } from "@/db/schema/accounts";
 
 export const scheduledPosts = sqliteTable(
     "scheduled_posts",
     {
         id: integer("id").primaryKey({ autoIncrement: true }),
         postContent: text("post_content").notNull(),
-        account: text("account").notNull(),
+        account: text("account")
+            .notNull()
+            .references(() => accounts.identifier),
         postOn: integer("post_on", { mode: "timestamp" }).notNull(),
         status: text("status").notNull().default("pending"),
         createdAt: integer("created_at", { mode: "timestamp" })
