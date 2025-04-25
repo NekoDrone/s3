d88cac4986aa0c343cf682f9a30a3115b1d98ff9
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2025-04-25 22:01:27 +0800

    feat: add accounts table with at rest encryption

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,9 +2,11 @@
 // any new schema definitions must be imported here before used in the rest of the app.
 
 import { scheduledPosts } from "@/db/schema/scheduled_posts";
+import { accounts } from "@/db/schema/accounts";
 
 export const schema = {
     scheduledPosts,
+    accounts,
 };
 
 export * from "@/db/schema/scheduled_posts";
---
+++ src/db/schema/accounts.ts
@@ -0,0 +1,25 @@
+import { index, integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { sql } from "drizzle-orm";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
+
+export const accounts = sqliteTable(
+    "accounts",
+    {
+        id: integer("id").primaryKey({ autoIncrement: true }),
+        identifier: text("account").notNull(),
+        appPasswordEncrypted: text("app_password_enc").notNull(),
+        appPasswordInitVec: text("app_password_init_vec").notNull(),
+        createdAt: integer("created_at", { mode: "timestamp" })
+            .notNull()
+            .default(sql`(CURRENT_TIMESTAMP)`),
+    },
+    (table) => {
+        return [index("idx_accounts_identifier").on(table.identifier)];
+    },
+);
+
+export const accountsSelectSchema = createSelectSchema(accounts);
+export const accountsInsertSchema = createInsertSchema(accounts);
+
+export type Account = z.infer<typeof accountsInsertSchema>;
