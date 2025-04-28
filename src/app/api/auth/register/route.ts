dc43513420a654808ac7bfb2900e6e11e12802f4
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2025-04-28 15:46:30 +0800

    feat: basic registration route

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,22 +1,37 @@
 import { accounts, accountsInsertSchema } from "@/db/schema/accounts";
 import db from "@/db";
+import { ApiResponse, StatusType } from "@/entities/types/responses";
 
 export async function POST(req: Request) {
     const body = await req.json();
     const validation = accountsInsertSchema.safeParse(body);
     if (validation.success) {
         await db.insert(accounts).values(validation.data);
-        return new Response(null, { status: 200, statusText: "OK" });
-    } else {
-        return new Response(
-            `Invalid data. Please ensure that you provide the correct type. Details: ${validation.error}`,
-            {
-                status: 400,
-                statusText: "Bad Request",
-                headers: {
-                    "Content-Type": "text/plain",
-                },
+        const response: ApiResponse = {
+            status: StatusType.SUCCESS,
+        };
+        return new Response(JSON.stringify(response), {
+            status: 200,
+            statusText: "OK",
+            headers: {
+                "Content-Type": "application/json",
             },
-        );
+        });
+    } else {
+        const response: ApiResponse = {
+            status: StatusType.ERROR,
+            error: {
+                message:
+                    "Invalid data. Please ensure that you provide the correct type.",
+                details: validation.error,
+            },
+        };
+        return new Response(JSON.stringify(response), {
+            status: 400,
+            statusText: "Bad Request",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
     }
 }
