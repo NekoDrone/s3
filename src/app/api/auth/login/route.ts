6c9a608179900485b4efeea677ec6c979cb83833
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2025-04-28 15:48:06 +0800

    feat: basic login handler

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -2,23 +2,71 @@ import { z } from "zod";
 import db from "@/db";
 import { and, eq } from "drizzle-orm";
 import { accounts } from "@/db/schema/accounts";
-import { LoginResponse } from '@/entities/types/responses'
+import {
+    ApiResponse,
+    LoginResponse,
+    StatusType,
+} from "@/entities/types/responses";
 
 export async function POST(req: Request) {
     const body = await req.json();
     const validation = loginOptsSchema.safeParse(body);
     if (validation.success) {
-        const appPassword: LoginResponse | undefined = await db.query.accounts.findFirst({
-            where: and(
-                eq(accounts.identifier, validation.data.identifier),
-                eq(accounts.passwordHash, validation.data.passwordHash),
-            ),
-            columns: {
-                appPasswordEncrypted: true,
-                appPasswordInitVec: true,
+        const data: LoginResponse | undefined =
+            await db.query.accounts.findFirst({
+                where: and(
+                    eq(accounts.identifier, validation.data.identifier),
+                    eq(accounts.passwordHash, validation.data.passwordHash),
+                ),
+                columns: {
+                    appPasswordEncrypted: true,
+                    appPasswordInitVec: true,
+                },
+            });
+        if (data) {
+            const response: ApiResponse = {
+                status: StatusType.SUCCESS,
+                data,
+            };
+            return new Response(JSON.stringify(response), {
+                status: 401,
+                statusText: "Unauthorized",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+        } else {
+            const response: ApiResponse = {
+                status: StatusType.ERROR,
+                error: {
+                    message:
+                        "Could not find provided username and password hash in database.",
+                },
+            };
+            return new Response(JSON.stringify(response), {
+                status: 401,
+                statusText: "Unauthorized",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+        }
+    } else {
+        const response: ApiResponse = {
+            status: StatusType.ERROR,
+            error: {
+                message:
+                    "Invalid data. Please ensure that you provide the correct type.",
+                details: validation.error,
+            },
+        };
+        return new Response(JSON.stringify(response), {
+            status: 400,
+            statusText: "Bad Request",
+            headers: {
+                "Content-Type": "application/json",
             },
         });
-        return;
     }
 }
 
