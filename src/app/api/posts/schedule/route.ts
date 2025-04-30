47effc0c2233cb357a139b7d3a1ba157cdd61abd
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2025-04-30 12:17:02 +0800

    feat, refactor: query account for id, early return

--- src/app/api/posts/schedule/route.ts
+++ src/app/api/posts/schedule/route.ts
@@ -1,48 +1,25 @@
 import db from "@/db";
-import { postsInsertSchema, scheduledPosts } from "@/db/schema/scheduled_posts";
+import {
+    ScheduledPostInsert,
+    scheduledPosts,
+} from "@/db/schema/scheduled_posts";
 import { ApiResponse, StatusType } from "@/entities/types/responses";
+import { z } from "zod";
+import { ErrorType } from "@/entities/types/errors";
+import { accounts } from "@/db/schema/accounts";
+import { eq } from "drizzle-orm";
 
 export async function POST(request: Request) {
     const body = await request.json();
-    const validation = postsInsertSchema.safeParse(body);
-    if (validation.success) {
-        try {
-            await db.insert(scheduledPosts).values(validation.data);
-        } catch (err) {
-            const response: ApiResponse = {
-                status: StatusType.ERROR,
-                error: {
-                    message:
-                        "Invalid DB transaction. Constraint mismatch? Usually means a foreign key doesn't exist. Check the inputs.",
-                    details: err,
-                },
-            };
-            return new Response(JSON.stringify(response), {
-                status: 400,
-                statusText: "Bad Request",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
-        }
-
-        const response: ApiResponse = {
-            status: StatusType.SUCCESS,
-        };
-        return new Response(JSON.stringify(response), {
-            status: 200,
-            statusText: "OK",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-    } else {
+    const { success, data, error } = schedulePostOptsSchema.safeParse(body);
+    if (!success) {
         const response: ApiResponse = {
             status: StatusType.ERROR,
             error: {
                 message:
                     "Invalid data. Please ensure that you provide the correct type.",
-                details: validation.error,
+                details: error,
+                type: ErrorType.TYPE_ERROR,
             },
         };
         return new Response(JSON.stringify(response), {
@@ -53,4 +30,62 @@ export async function POST(request: Request) {
             },
         });
     }
+
+    const { identifier, postOn, postContent } = data;
+
+    const account = (
+        await db
+            .select({
+                id: accounts.id,
+                identifier: accounts.identifier,
+            })
+            .from(accounts)
+            .where(eq(accounts.identifier, identifier))
+            .limit(1)
+    )[0];
+
+    const insertionData: ScheduledPostInsert = {
+        account: account.id,
+        postOn,
+        postContent,
+    };
+
+    try {
+        await db.insert(scheduledPosts).values(insertionData);
+    } catch (err) {
+        const response: ApiResponse = {
+            status: StatusType.ERROR,
+            error: {
+                message:
+                    "Invalid DB transaction. Constraint mismatch? Usually means a foreign key doesn't exist. Check the inputs.",
+                details: err,
+            },
+        };
+        return new Response(JSON.stringify(response), {
+            status: 400,
+            statusText: "Bad Request",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    }
+
+    const response: ApiResponse = {
+        status: StatusType.SUCCESS,
+    };
+    return new Response(JSON.stringify(response), {
+        status: 200,
+        statusText: "OK",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
 }
+
+const schedulePostOptsSchema = z.object({
+    identifier: z.string(),
+    postOn: z.coerce.date(),
+    postContent: z.string(),
+});
+
+export type SchedulePostOpts = z.infer<typeof schedulePostOptsSchema>;
