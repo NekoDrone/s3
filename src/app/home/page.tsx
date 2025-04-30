9a29d4ffc9b7c765a38ba2f3e8c9d21b49cf0112
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2025-04-30 19:38:52 +0800

    feat: home page shell

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,6 +5,8 @@ import { redirect, RedirectType } from "next/navigation";
 import { UserData } from "@/entities/types/client";
 import { SchedulePost } from "@/components/Home/SchedulePost";
 import { Footer } from "@/components/Misc/Footer";
+import { PostsList } from "@/components/Home/PostsList";
+import { ProfileReadout } from "@/components/Home/ProfileReadout";
 
 const Home = () => {
     const userData = useLocalStorage<UserData>("userData")[0];
@@ -14,8 +16,12 @@ const Home = () => {
 
     return (
         <div className="bg-ctp-base flex min-h-screen flex-col items-center justify-center gap-8">
-            <p>S3</p>
-            <p>Your scheduled skeets</p>
+            <p className="text-lg font-medium">
+                Serenity&#39;s Skeet Scheduler
+            </p>
+            <ProfileReadout />
+
+            <PostsList />
             <SchedulePost />
             <Footer />
         </div>
