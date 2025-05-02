e5aadc09e83dc9281a13f557cb42505615e00dc5
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2025-05-02 20:41:41 +0800

    refactor: redo favicon, branding

--- src/components/Misc/Footer.tsx
+++ src/components/Misc/Footer.tsx
@@ -5,14 +5,7 @@ export const Footer = () => {
     return (
         <div className="flex items-center justify-center gap-2">
             <div className="flex gap-1">
-                <p className="text-sm font-light">Built with</p>
-                <Image
-                    src="https://emoji.lgbt/assets/svg/lesbian-heart.svg"
-                    alt="Lesbian heart!"
-                    width="16"
-                    height="16"
-                />
-                <p className="text-sm font-light">by </p>
+                <p className="text-sm font-light">Built with 💜 by</p>
                 <a
                     className="hover:text-ctp-lavender text-sm font-light underline"
                     href="https://serenity.tgirl.gay"
